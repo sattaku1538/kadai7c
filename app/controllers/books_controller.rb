42c960e16ba0df1f6d296c2e220e3256124e5bc9
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-09 07:14:21 +0000

    fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     # @books = Book.all　投稿したものを表示する。
     # @books = Book.all.sort {|a,b| b.favorites.count <=> a.favorites.count} いいね数の順番に投稿を表示。
     to  = Time.current.at_end_of_day
-    from  = (to - 1.day).at_beginning_of_day
+    from  = (to - 6.day).at_beginning_of_day
     @books = Book.includes(:favorited_users).
       sort {|a,b| 
         b.favorited_users.includes(:favorites).where(created_at: from...to).size <=> 
